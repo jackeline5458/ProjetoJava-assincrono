abbebad40a5dde6bb7c279f3bf4a8dc5276b8f18
jackeline <jackelinepessoa0@gmail.com>
2026-04-17 10:28:20 -0300

    Primeiro commit

---
+++ src/Intoducao/HelloWord.java
@@ -0,0 +1,10 @@
+package Intoducao;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		
+		System.out.print("hello world!");
+	}
+
+}
---
+++ src/Intoducao/VariaveisConstantes.java
@@ -0,0 +1,10 @@
+package Intoducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
