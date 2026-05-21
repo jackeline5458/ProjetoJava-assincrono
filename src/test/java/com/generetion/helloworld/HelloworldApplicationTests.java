f5fcdd3af922f4ed281700319f35ccdc7d4ef72c
jackeline <jackelinepessoa0@gmail.com>
2026-05-21 19:41:38 -0300

    Atividade Hello World Spring

---
+++ src/test/java/com/generetion/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generetion.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
